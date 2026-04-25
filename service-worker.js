5247312e2ea1698ae17ff677118662ca9185c17b
Ujjwal Kumar Bhowmick <ujjwalkumarbhowmick30@gmail.com>
2026-04-25 18:09:02 +0530

    Fix: Removed restrictive headers and updated Service Worker to v6 for guaranteed app visibility and performance

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
  * Matdan Sathi - Election Process Education Assistant
  * Developer - Ujjwal Kumar Bhowmick (ujjwalkumarbhowmick30@gmail.com)
  */
-const CACHE_NAME = 'matdan-sathi-v5';
+const CACHE_NAME = 'matdan-sathi-v6';
 const ASSETS = [
     './',
     './index.html',
